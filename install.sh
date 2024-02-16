bcf012b3f4bee27af1b0d10d602ebc1d19006073
KC OConnor <kc.oconnor@gmail.com>
2024-02-16 02:51:48 +0000

    chore: remove gitignore file

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt update
 sudo apt install -y stow
-[ -e ${HOME}/.gitignore ] && rm ${HOME}/.gitignore
 [ -e ${HOME}/.profile ] && rm ${HOME}/.profile
 [ -e ${HOME}/.zprofile ] && rm ${HOME}/.zprofile
 [ -e ${HOME}/.zshrc ] && rm ${HOME}/.zshrc
