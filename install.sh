e7e8dd9cd2d9019fff98dc10177639369c4adf57
KC OConnor <kc.oconnor@gmail.com>
2024-02-16 02:43:16 +0000

    chore: add gitignore and zsh config

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo apt update
 sudo apt install -y stow
-cd ~/dotfiles
+[ -e ${HOME}/.gitignore ] && rm ${HOME}/.gitignore
+[ -e ${HOME}/.profile ] && rm ${HOME}/.profile
+[ -e ${HOME}/.zprofile ] && rm ${HOME}/.zprofile
+[ -e ${HOME}/.zshrc ] && rm ${HOME}/.zshrc
+cd ${HOME}/dotfiles
 stow .
-
